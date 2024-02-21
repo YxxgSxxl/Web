0c593f6217bd1837fe256532ecfad19e49f12a46
YxxgSxxl <eckes.alex14@gmail.com>
2024-02-21 22:24:47 +0100

    Canvas init

--- canvas-particles/js/script.js
+++ canvas-particles/js/script.js
@@ -1,5 +1,26 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const particules = new Particules();
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+
+
+// class Particules {
+//     constructor() {
+//         this.x = 100;
+//         thix.y = 200;
+//         thix.vX = 2;
+//         this.vY = 2;
+//         this.gravity = 0.8;
+//     }
+
+//     draw() {
+//         ctx.fillStyle("red");
+//         ctx.fillRect(this.x, this.y, 50, 50);
+//     }
+// }
+
+// particules.draw();
